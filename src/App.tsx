1554308892056e4add8d9ad70fff78ab36b52711
Tong Quang Truong <truongwerk@gmail.com>
2022-07-10 23:34:18 +0700

    WIP: Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
+
 const App = () => {
 	return (
-		<div className="app">
-			<h1>Tong Quang Truong</h1>
-		</div>
+		<Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/exercise/:id" element={<ExerciseDetail />} />
+			</Routes>
+			<Footer />
+		</Box>
 	);
 };
 
